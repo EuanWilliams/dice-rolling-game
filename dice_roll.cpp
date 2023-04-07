1e134fcbff406c90d7b64eb5b77e1e94235cd15c
Euan Williams <euan.williams01061999@gmail.com>
2023-04-07 14:37:44 +0100

    implemented player class

--- dice_roll.cpp
+++ dice_roll.cpp
@@ -4,28 +4,45 @@
 #include <cstdlib>
 using namespace std;
 
+class player {
+    public: 
+        string name;
+        int score;
+};
+
 void rolling_output(){
     cout << "Rolling.";
     this_thread::sleep_for(chrono::milliseconds(300));
     cout << ".";
     this_thread::sleep_for(chrono::milliseconds(300));
     cout << "." << endl;
-}
+};
 
 int main() {
-    string player_one_name;
+    player player_one; 
     cout << "Enter player one name: ";
-    cin >> player_one_name;
-    cout << "Player ones name: " << player_one_name << endl;
+    cin >> player_one.name;
 
-    string player_two_name;
+    player player_two;
     cout << "Enter player two name: ";
-    cin >> player_two_name;
-    cout << "Player two name: " << player_two_name << endl;
+    cin >>  player_two.name;
 
     rolling_output();
-    
+
     srand((unsigned) time(NULL));
-    int random = 1 + (rand() % 6);
-    cout << "Random number " << random << endl;     
-}
+    player_one.score = 1 + (rand() % 6);
+    cout << player_one.name << " rolled a: " << player_one.score << endl;
+
+    rolling_output();
+
+    srand((unsigned) time(NULL));
+    player_two.score = 1 + (rand() % 6);
+    cout << player_two.name << " rolled a: " << player_two.score << endl;
+
+    if (player_one.score > player_two.score) {
+        cout << player_one.name << " won.";
+    }
+    else {
+        cout << player_two.name << " won.";
+    }
+};
